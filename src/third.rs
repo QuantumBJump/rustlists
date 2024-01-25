909acb69bd72ee60f259504f53c4e93ff129e033
Quinn Stevens <quinn@thoughtmachine.net>
2024-01-25 13:17:25 +0000

    persistent stack prepend

---
+++ src/third.rs
@@ -0,0 +1,28 @@
+use std::rc::Rc;
+
+pub struct List<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    // Prepend takes a list and an element, returns the list with that element at the front
+    pub fn prepend(&self, elem: T) -> List<T> {
+        List {
+            head: Some(Rc::new(Node {
+                elem,
+                next: self.head.clone(),
+            }))
+        }
+    }
+}
